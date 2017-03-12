0c65f3a1afd330816b9e89435d0def29cde122c9
Sergii Varenyk <seva0716@netcracker.com>
2017-03-12 11:12:51 +0200

    first commit

---
+++ part1.py
@@ -0,0 +1,20 @@
+''' Exercise 1.10.3.1. Write a version, add3.py, that asks for three numbers, and lists all three, and
+their sum, in similar format to the example above.'''
+
+a = int(input("Enter the first integer, please: "))
+b = int(input("Enter the second integer, please: "))
+c = int(input("Enter the third integer, please: "))
+print('The first integer is:', a)
+print('The second integer is:', b)
+print('The third integer is:', c)
+print('The sum of',a,'and',b,'and',c,'is:', a+b+c)
+
+'''Exercise 1.10.3.2. a. Write a program, quotient.py, that prompts the user for two integers, and
+then prints them out in a sentence with an integer division problem like "The quotient of 14 and 3 is
+4 with a remainder of 2".'''
+
+d = int(input("Enter the first integer, please: "))
+e = int(input("Enter the second integer, please: "))
+print('The quotient of', d, 'and', e, 'is', d//e, 'with a remainder of', d%e)
+
+''' the next '''
